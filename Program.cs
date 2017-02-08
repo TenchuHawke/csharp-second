bbd8990f2a34d38b8ba743d3fda9e3fcdec517e3
TenchuHawke <hawke@unrestrainedgames.com>
2017-02-08 15:04:47 -0800

    Stuff

--- Program.cs
+++ Program.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 
 namespace ConsoleApp {
     public class Program {
-        public static void SayHello(string name)
-        {
-            System.Console.WriteLine("Hello to you, {0}", name);
+        public static void SayHello (string name) {
+            System.Console.WriteLine ("Hello to you, {0}", name);
         }
-        public static void Main(){
-            SayHello("Jeff");
+        public static void Main () {
+            SayHello ("Jeff");
         }
 
     }
